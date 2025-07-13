8eb1d69e127113678a21a42512cd610dad362d83
Hampus Hallkvist <hampus@hallkvist.org>
2025-07-13 11:24:03 +0200

    add qemu scripts and logging

---
+++ run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Set default arguments if not provided
+KERNEL_DIR=${1:-.}
+IMAGE_DIR=${2:-image}
+
+# Start the QEMU instance
+echo "Starting QEMU..."
+
+../qemu/build/qemu-system-x86_64 \
+    -m 4G \
+    -smp 8 \
+    -kernel "$KERNEL_DIR/arch/x86/boot/bzImage" \
+    -append "console=ttyS0 root=/dev/sda earlyprintk=serial net.ifnames=0 nokaslr" \
+    -drive file="$IMAGE_DIR/bookworm.img",format=raw \
+    -drive file="$IMAGE_DIR/blknvme",if=none,id=mynvme,format=raw \
+    -device nvme,drive=mynvme,serial=deadbeef,id=nvme0,x-tio=on,spdm_port=2323 \
+    -net user,host=10.0.2.10,hostfwd=tcp:127.0.0.1:10021-:22 \
+    -net nic,model=e1000 \
+    -trace events=./qemu-trace-events \
+    -trace file=qemu-instance.log \
+    -serial none \
+    -monitor stdio \
+    -enable-kvm \
+    -nographic \
+    -pidfile vm.pid \
+    -machine q35 \
+    -qmp unix:/tmp/qmp-sock,server,nowait
