e2b3917f6a583d5c8333abcc37b45af1b7953862
Hampus Hallkvist <hampus@hallkvist.org>
2025-07-21 14:13:24 +0200

    use aio=native and cache=none This is done in the hopes to increase determinism

--- run-shell.sh
+++ run-shell.sh
@@ -13,15 +13,18 @@ echo "Starting QEMU..."
     -kernel "$KERNEL_DIR/arch/x86/boot/bzImage" \
     -append "console=ttyS0 root=/dev/sda earlyprintk=serial net.ifnames=0 nokaslr" \
     -drive file="$IMAGE_DIR/bookworm.img",format=raw \
-    -drive file="$IMAGE_DIR/blknvme",if=none,id=mynvme,format=raw \
-    -device nvme,drive=mynvme,serial=deadbeef,id=nvme0,x-tio=on,spdm_port=2323 \
+    -drive file="$IMAGE_DIR/blknvme",if=none,id=mynvme,format=raw,aio=native,cache=none \
+    -device nvme,drive=mynvme,serial=deadbeef,id=nvme0,x-tio=off,spdm_port=2323 \
     -net user,host=10.0.2.10,hostfwd=tcp:127.0.0.1:10021-:22 \
     -net nic,model=e1000 \
-    -trace enable=nvme_dma_mode_enabled \
-    -trace enable=nvme_dma_mode_disabled \
     -trace enable=nvme_bounce_buffer_copy_error \
+    -trace enable=nvme_bounce_buffer_copy_complete \
     -trace file=qemu-instance.log \
     -enable-kvm \
     -nographic \
     -pidfile vm.pid \
     -machine q35
+
+
+#-trace enable=nvme_dma_mode_enabled \
+#-trace enable=nvme_dma_mode_disabled \
